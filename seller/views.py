297f0520562b4d51ab5fe9a736846bfd7f884929
CrypTe <102381890+GamingSeries@users.noreply.github.com>
2023-02-09 10:50:43 +0400

    seller_form created

--- seller/views.py
+++ seller/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ItemForm
 
 # Create your views here.
 
 def acc_view(request):
-    return render (request, 'seller/acc_view.html')
+    form = ItemForm()
+    
+    if request.method == 'POST':
+        data = request.POST
+        form = ItemForm(data)
+        if form.is_valid():
+            form.save()
+            redirect('exfore : home.html')
+        else:
+            print(form.errors)
+    return render (request, 'seller/acc_view.html', {'form' : form})
