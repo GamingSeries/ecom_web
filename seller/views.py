a5dd5cd32a92ebaf4324a023a99d9368e1216a1c
CrypTe <102381890+GamingSeries@users.noreply.github.com>
2023-02-11 18:43:11 +0400

    view changed

--- seller/views.py
+++ seller/views.py
@@ -7,12 +7,10 @@ def acc_view(request):
     form = ItemForm()
     
     if request.method == 'POST':
-        data = ItemForm(request.POST, request.FILES)
-        form = ItemForm(data)
+        form = ItemForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            redirect('exfire : home.html')
-
-        else:
-            print(form.errors)
-    return render (request, 'seller/acc_view.html', {'form' : form})
+    else:
+        print(form.errors)
+    
+    return render(request, 'seller/acc_view.html', {'form': form})
