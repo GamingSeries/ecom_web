7a864a99eb896a3cfb0ecdd2d7198d3fa93b745f
CrypTe <102381890+GamingSeries@users.noreply.github.com>
2023-02-11 22:13:30 +0400

    seller_register added

--- seller/models.py
+++ seller/models.py
@@ -2,6 +2,22 @@ from django.db import models
 
 # Create your models here.
 
+class Seller_Register(models.Model):
+    seller_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = 'Seller_Register'
+        verbose_name_plural = 'Seller_Registers'
+
+    def __str__(self):
+        return self.name
+
 class Item(models.Model):
     item_number = models.IntegerField(primary_key= True)
     name = models.CharField(max_length=100)
@@ -15,4 +31,3 @@ class Item(models.Model):
 
     def __str__(self):
         return self.name
-    
